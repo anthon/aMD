a615ffb7bdfa1be68a2a775d38a9d869e65877e9
Anthon Astrom <anthon@astromzimmer.com>
2016-02-15 13:01:41 +0100

    Adding MAX_FILE_SIZE to file inputs

--- aMD.Converter.js
+++ aMD.Converter.js
@@ -42,7 +42,7 @@
         required = required ? 'required' : '';
         result = '<fieldset class="' + required + '">';
         result += '<legend>' + label + '</legend>';
-        result += '<input name="MAX_FILE_SIZE" type="hidden" value="' + (size * 1024 * 1024) + '">';
+        result += '<input name="MAX_FILE_SIZE" type="hidden" value="' + (parseInt(size) * 1024 * 1024) + '">';
         result += '<input name="' + name + '" type="file" accept="' + accept + '">';
         result += '</fieldset>';
         return result;
