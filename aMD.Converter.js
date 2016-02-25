c9bdaef69b3161f9953637c587c4b689813d960d
Anthon Astrom <anthon@astromzimmer.com>
2016-02-25 14:38:53 +0100

    Adding type to input

--- aMD.Converter.js
+++ aMD.Converter.js
@@ -5,7 +5,7 @@
   init = function() {
     aMD.md = new Markdown.Converter();
     aMD.md.hooks.chain('preConversion', function(text) {
-      return text.replace(/(\w[\w\/ \t\-\?\(\))]*)(\*)?[ \t]*=[ \t]_([_\w]+)?_(\[(\d+)?\])?(\(([\wa-zA-Z\u00E0-\u017F\.,'\?\!\/ \t\-]+)\))?/g, function(whole, label, required, type, _maxlength, maxlength, _placeholder, placeholder) {
+      return text.replace(/(\w[\w\/ \t\-\?\(\))]*)(\*)?[ \t]*=[ \t]_([^_\w]+|_)?_(\[(\d+)?\])?(\(([\wa-zA-Z\u00E0-\u017F\.,'\?\!\/ \t\-]+)\))?/g, function(whole, label, required, type, _maxlength, maxlength, _placeholder, placeholder) {
         var name, result, size;
         label = label.trim().replace(/\t/g, ' ');
         name = label.trim().replace(/[ \t]/g, '-').toLowerCase();
